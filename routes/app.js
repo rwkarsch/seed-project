6aef663b956349d9d5e2ac12c523c774b5740e9b
rwkarsch <robertkarsch@gmail.com>
2017-11-12 12:15:53 -0500

    First commit?

---
+++ routes/app.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+var User = require('../models/user');
+
+router.get('/', function (req, res, next) {
+   User.findOne({}, function (err, doc){
+   		if (err){
+   			return res.send('Error!')
+   		}
+   		res.render('node',{email: doc.email	})	
+	})
+});
+
+router.post('/', function (req, res, next) {
+    var email = req.body.email;
+    var user = new User({
+    	firstName: 'Max',
+    	lastName: 'Schwartz',
+    	password: 'super-secret',
+    	email: email
+    });
+    user.save();
+    res.redirect('/');
+});
+
+
+module.exports = router;
